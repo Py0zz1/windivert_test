263cbd5d5ba602aabbb2661e1e1ec82458a632a5
Py0zz1 <corny_@naver.com>
2018-05-09 09:41:30 +0900

    header_length fixed

--- windivert_test.cpp
+++ windivert_test.cpp
@@ -7,11 +7,48 @@
 
 #define BUF_SIZE 65535
 
-typedef struct
+struct ip_header
 {
-	WINDIVERT_IPHDR ip;
-	WINDIVERT_TCPHDR tcp;
-} TCPPACKET, *PTCPPACKET;
+	unsigned char ip_header_length : 4;
+	unsigned char ip_version : 4;
+	unsigned char ip_TOS;
+	unsigned short ip_total_length;
+	unsigned short ip_iden;
+	unsigned char flag_x : 1;
+	unsigned char flag_D : 1;
+	unsigned char flag_M : 1;
+	unsigned char offset_part_1 : 5;
+	unsigned char offset_part_2;
+	unsigned char TTL;
+	unsigned char ip_protocol;
+	unsigned short chk_sum;
+	struct in_addr ip_src_add;
+	struct in_addr ip_des_add;
+	//20bytes
+};
+
+struct tcp_header
+{
+	unsigned short src_port;
+	unsigned short des_port;
+	unsigned long sqn_num; 
+	unsigned long ack_num;
+	unsigned char offset : 4;
+	unsigned char ns : 1;
+	unsigned char reserve : 3;
+	unsigned char flag_cwr : 1;
+	unsigned char flag_ece : 1;
+	unsigned char flag_urgent : 1;
+	unsigned char flag_ack : 1;
+	unsigned char flag_push : 1;
+	unsigned char flag_reset : 1;
+	unsigned char flag_syn : 1;
+	unsigned char flag_fin : 1;
+	unsigned short window;
+	unsigned short chk_sum;
+	unsigned short urgent_point;
+	//20bytes
+};
 
 int main(int argc, char **argv)
 {
@@ -21,6 +58,8 @@ int main(int argc, char **argv)
 	unsigned char packet[BUF_SIZE];
 	UINT pkt_len;
 	const char *err_buf;
+	ip_header *ih;
+	tcp_header *th;
 	
 	handle = WinDivertOpen("true", WINDIVERT_LAYER_NETWORK, priority, 0);
 
@@ -47,12 +86,14 @@ int main(int argc, char **argv)
 			continue;
 		}
 
-		PTCPPACKET tcp = (TCPPACKET *)packet;
+		ih = (ip_header *)packet;
+		th = (tcp_header *)(packet + ih->ip_header_length * 4);
+		
 
-		if (ntohs(tcp->tcp.DstPort) == 80 || ntohs(tcp->tcp.SrcPort) == 80)
+		if (ntohs(th->des_port) == 80 || ntohs(th->src_port) == 80)
 		{
-			printf("Dst_Port = %d\n", ntohs(tcp->tcp.DstPort));
-			printf("Src_Port = %d\n", ntohs(tcp->tcp.SrcPort));
+			printf("Dst_Port = %d\n", ntohs(th->des_port));
+			printf("Src_Port = %d\n", ntohs(th->src_port));
 			printf("====================================\n");
 			continue;
 		}
